630ba96f66924de428d10926c25f83ecbc24d758
Smart  Raysam <engsamtob@yahoo.com>
2024-01-03 22:36:27 +0100

    Task 6 final user

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,22 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const user = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(fileName);
+
+  return Promise.allSettled([user, photo]).then((data) => {
+    const array = [];
+    data.forEach((element) => {
+      if (element.status === 'fulfilled') {
+        array.push(element.value);
+      } else {
+        array.push({
+          status: element.status,
+          value: `Error: ${element.reason.message}`,
+        });
+      }
+    });
+    return array;
+  });
+}
