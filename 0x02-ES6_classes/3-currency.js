38d57921176adb0b83deb86e1825930c7002bc56
Smart  Raysam <engsamtob@yahoo.com>
2024-01-04 18:08:03 +0100

    task 3 currency

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,38 @@
+export default class Currency {
+  // eslint-disable-line
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new TypeError('code must be a String');
+    } else if (typeof name !== 'string') {
+      throw new TypeError('name must be a String');
+    }
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    if (typeof newCode !== 'string') {
+      throw new TypeError('code must be a String');
+    }
+    this._code = newCode;
+  }
+
+  get name() {
+    return this._code;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('name must be a String');
+    }
+    this._name = newName;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+} // eslint-disable-line
