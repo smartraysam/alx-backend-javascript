6fc1ecc21c745ff2a1586d11b3672c70b9917d20
Smart  Raysam <engsamtob@yahoo.com>
2024-01-09 21:55:18 +0100

    update task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,16 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  if (Array.isArray(students)) {
-    const studentsByCity = students.filter((obj) => obj.location === city);
-    const studentsIds = studentsByCity.map((obj) => obj.id);
-    const newStudents = students.map((obj) => {
-      if (studentsIds.includes(obj.id)) {
-        const newGrade = newGrades.filter((gradeObj) => gradeObj.studentId === obj.id);
-        return { ...obj, grade: newGrade[0].grade };
-      }
-      return obj;
-    });
-    return newStudents;
+  if (Object.getPrototypeOf(students) !== Array.prototype) {
+    return [];
   }
-  return [];
+  if (Object.getPrototypeOf(newGrades) !== Array.prototype) {
+    return [];
+  }
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const [newGrade] = newGrades.filter(
+        (obj) => obj.studentId === student.id
+      );
+      return { ...student, grade: newGrade ? newGrade.grade : 'N/A' };
+    });
 }
